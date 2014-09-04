9041e09511d3d4588a9951579210b705972f82e9
Amelia Laundy <apprentice@Samoa.local>
2014-09-04 16:24:35 +1200

    log out button done

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,11 +1,23 @@
 get "/" do
   @user = User.find_by_name(session[:user])
-  erb :index
+  if @message = session[:message]
+    session[:message] = nil
+  end
+     erb :index
 end
 
 post "/sign-in" do
   @user = User.find_by_name(params[:name])
-  session[:user] = @user.name if @user
-
+  if @user
+    session[:user] = @user.name
+  else
+    puts "else section"
+    session[:message] = "invalid user name"
+  end
   redirect to '/'
+end
+
+get "/sign-out" do
+  session[:user] = nil
+  erb :index
 end
