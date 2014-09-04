630e795565497f1477829d6ff20834e623b25850
Amelia Laundy <apprentice@Samoa.local>
2014-09-05 09:45:31 +1200

    fixing view merge conflict with fadeout

--- public/js/view.js
+++ public/js/view.js
@@ -6,12 +6,10 @@ function View(user, rocket)
   this.rocketSelector = ".rocketship";
   this.islandSelector = ".island-image"
   this.startButtonSelector = "#start";
-<<<<<<< HEAD
   this.stopButtonSelector = "#stop";
-=======
   this.controlPanelSelector = "#control-panel";
   this.gameNameSelector = "#game-name";
->>>>>>> master
+
 }
 
 View.prototype = {
@@ -28,17 +26,14 @@ View.prototype = {
 
   startButton: function() {
     $(this.rocketSelector).addClass("active");
-<<<<<<< HEAD
-    $(this.islandSelector).addClass("active");
+    $(this.islandSelector).addClass("active").animate({left: "+=250"}); //moves island in from the left by 250, animates css property (in css its set to -200)
+    $(this.controlPanelSelector).fadeOut(1000);
+    $(this.gameNameSelector).delay(1000).fadeOut(1000);
   },
 
   stopButton: function() {
     this.updateRocketPosition()
-=======
-    $(this.islandSelector).addClass("active").animate({left: "+=250"}); //moves island in from the left by 250, animates css property (in css its set to -200)
-    $(this.controlPanelSelector).fadeOut(1000);
-    $(this.gameNameSelector).delay(1000).fadeOut(1000);
->>>>>>> master
+
   }
 
 }
